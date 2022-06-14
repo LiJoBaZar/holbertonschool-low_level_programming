bbcd8a934765054dbcd24fadac89f7d0b551cdfc
LiJoBaZar <lijobazar@gmail.com>
2022-06-14 16:58:37 -0700

    bbqu

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main - prints a given the phrase
- *Return: 1
+ *main - prints exactly (and that piece of art is useful)
+ *- Dora Korpar...
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 59);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

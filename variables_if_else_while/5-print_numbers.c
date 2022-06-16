ea07e50cbe98f283ce827ddd70d9832608b692fb
LiJoBaZar <lijobazar@gmail.com>
2022-06-16 10:46:02 -0700

    fuckbetty

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - prints the alphabet using putchar
+ *Return: 0 if success
+ */
+	int main(void)
+{
+		int n = 0;
+			while (n < 10)
+	{
+				printf("%d", n);
+				n++;
+	}
+				printf("\n");
+			return (0);
+}

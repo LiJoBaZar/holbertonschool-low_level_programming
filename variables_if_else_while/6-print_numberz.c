66faeea6210327fd8f4db8c3127434e6a68b9121
LiJoBaZar <lijobazar@gmail.com>
2022-06-16 10:58:12 -0700

    numberprint

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - prints the numbers from 0 to 9 using putchar
+ *Return: 0 if success
+ */
+int main(void)
+{
+		int n = 48;
+			while (n < 58)
+		{
+				putchar(n);
+				n++;
+		}
+				putchar('\n');
+			return (0);
+}

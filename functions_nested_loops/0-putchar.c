2e18333267bda80cab5f486fef51c1a8b531786c
LiJoBaZar <lijobazar@gmail.com>
2022-06-17 09:01:52 -0700

    putc

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
- *main - printf _putchar
+ *main - prints _putchar
  *Return: 0 if success
  */
 int main(void)
 {
-		printf("_putchar");
+		_putchar(95);
+		_putchar(112);
+		_putchar(117);
+		_putchar(116);
+		_putchar(99);
+		_putchar(104);
+		_putchar(97);
+		_putchar(114);
+		_putchar(10);
 		return (0);
 }

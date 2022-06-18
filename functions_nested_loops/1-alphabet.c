357810104b1f0194fdfe576ba318ef2ec16821f0
LiJoBaZar <lijobazar@gmail.com>
2022-06-17 20:19:44 -0700

    fort

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet - entry point
+ *
+ *Description: Prints the alphabet with _putchar
+ *
+ *Return: void
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+}
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,17 +1,6 @@
-#ifndef MAIN_H_   /* include guard */
-#define MAIN_H_
+#include "main.h"
 
-int _putchar(char c);
-void print_alphabet(void);  /* an example function declaration */
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-#endif
+int main(void){
+	_putchar(int x);
+	return(0);
+}

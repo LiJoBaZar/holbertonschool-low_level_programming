2c8237a9cd927dc4ecd2f19aed0b4035fb9e7022
LiJoBaZar <lijobazar@gmail.com>
2022-06-17 21:46:01 -0700

    loops

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_sign - prints the alphabet using putchar
+ *@n: is a parameter
+ *Return: 1 if c is lowercase, 0 if it isnt
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);						
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);						
+	}
+}

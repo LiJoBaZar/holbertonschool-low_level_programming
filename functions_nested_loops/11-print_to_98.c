f8e84ef3764b502bac2959966687e48aa1d99fbd
LiJoBaZar <lijobazar@gmail.com>
2022-06-22 17:00:17 -0700

    'cuote'

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,34 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- *Print_to_98 - prints all natural numbers from n to 98
+ *Print_to_98 - counting
+ *@n: number
+ *
  *Return: nothing, finction void
- *@n: is a parameter
  */
 void print_to_98(int n)
 {
-		int i = 0;
+	int i, j;
 
-			if (n < 98)
-			{
-				for (i = n ; i < 98 ; i++)
-					printf("%d, ", i);
-					printf("%d\n", 98);
-			}
-				else if (n == 98)
-					printf("%d\n", 98);
-				else
-					{
-				for (i = n ; i > 98 ; i--)
-					printf("%d, ", i);
-					printf("%d\n", 98);
-					}
-				return;
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	}
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
 }

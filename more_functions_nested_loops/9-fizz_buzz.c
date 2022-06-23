45d5e5a81df13115e44ffd13b0526f5c69af56aa
LiJoBaZar <lijobazar@gmail.com>
2022-06-23 12:49:08 -0700

    fizz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Challenge: the Fizz- Buzz
+ *Return: 0 if sucefull
+ */
+int main(void)
+
+{
+	int i;
+
+		for (i = 1; i <= 100; i++)
+		{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+		}
+			printf("\n");
+		return (0);
+}

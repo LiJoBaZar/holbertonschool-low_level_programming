8dd4e0ac32efee8fc3404e655a1dd62db92a5328
LiJoBaZar <lijobazar@gmail.com>
2022-06-22 20:35:03 -0700

    loops

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_isupper - Returns 1 if is upper 0 if not
+ *
+ *@c: is a parameter
+ *
+ *Return: 1 if c is lowercase, 0 if it isnt
+ */
+int _isupper(int c)
+
+{
+		return (c >= 'A' && c <= 'Z');
+}

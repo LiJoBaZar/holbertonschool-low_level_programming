a25c35220e574f35485a0c758b13e9a5e3be68b9
LiJoBaZar <lijobazar@gmail.com>
2022-06-23 10:09:59 -0700

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+nclude "holberton.h"
+
+/**
+ *print_square - prints a square in the terminal
+ *@size: is a parameter
+ */
+void print_square(int size)
+
+{
+	int i, j;
+
+		if (size <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+		for (i = 0; i < size ; i++)
+		{
+		for (j = 0; j < size ; j++)
+		{
+			_putchar('#');
+		}
+			_putchar('\n');
+		}
+		}
+}

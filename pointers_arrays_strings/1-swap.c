32d8fe572cc72519fc3e67c8824b7c58009ad62b
LiJoBaZar <lijobazar@gmail.com>
2022-06-28 10:34:24 -0700

    loveit

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps the values of two integers
+ *@a: is a parameter
+ *@b: is a parameter
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = c;
+}

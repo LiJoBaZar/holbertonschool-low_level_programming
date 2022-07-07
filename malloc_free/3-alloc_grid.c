56d335751c82a40d08e4adb05bbd605a51fc34c8
LiJoBaZar <lijobazar@gmail.com>
2022-07-07 07:34:57 -0700

    pop

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *alloc_grid - Returns a pointer to a 2 dimensional array of integers
+ *@width: width of matrix
+ *@height: height of matrix
+ *Return: Pointer of the concatenated strings
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+
+	arr = (int **)malloc(height * sizeof(int *));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *)malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (j = 0; j <= height; j++)
+			{
+				free(arr[j]);
+			}
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+} 	

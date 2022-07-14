48e3df215c374b6546f07a66492a2eef11dce1c2
LiJoBaZar <lijobazar@gmail.com>
2022-07-13 21:17:19 -0700

    poppy

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *print_dog - Print dog info
+ *@d: Pointer to dog struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}

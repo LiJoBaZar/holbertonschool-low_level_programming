cbedba2d868e4f1ba3219ac24535c472be802a8e
LiJoBaZar <lijobazar@gmail.com>
2022-07-26 18:37:03 -0700

    Add logic to list/len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *list_len - Returns the number of elements in a linked list_t list
+ *@h: Pointer to the node to print
+ *Return: Number number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+		return (0);
+
+	for (i = 0; h; i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}

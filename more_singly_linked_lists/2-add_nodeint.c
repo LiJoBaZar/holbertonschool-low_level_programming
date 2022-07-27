c1c83d77a45a13a00b608b0c52c6af65c30156f2
LiJoBaZar <lijobazar@gmail.com>
2022-07-27 16:49:24 -0700

    Add logic at add_nodeint

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *add_nodeint - Adds a new node at the beginning of a list_t list
+ *@head: Pointer to the node to print
+ *@n: Number to be added to the node
+ *Return: Added node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}

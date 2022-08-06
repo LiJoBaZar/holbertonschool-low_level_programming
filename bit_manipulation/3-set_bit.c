eef0dd81f93e70ecc93f7ee64575ae3bd4566ac6
LiJoBaZar <lijobazar@gmail.com>
2022-08-05 17:48:00 -0700

    Add logic at set bit

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,23 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- *main - check the code
- *
- *Return: Always 0.
+ *set_bit - Sets the value of a bit to 1 at a given index
+ *@n: Number
+ *@index: index of the bit you want to set to 1
+ *Return: 1 if worked otherwise -1
  */
-int main(void)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int n;
-
-	n = 1024;
-	set_bit(&n, 5);
-	printf("%lu\n", n);
-	n = 0;
-	set_bit(&n, 10);
-	printf("%lu\n", n);
-	n = 98;
-	set_bit(&n, 0);
-	printf("%lu\n", n);
-	return (0);
+	if (index > 63)
+		return (-1);
+	*n = ((1 << index) | *n);
+	return (1);
 }

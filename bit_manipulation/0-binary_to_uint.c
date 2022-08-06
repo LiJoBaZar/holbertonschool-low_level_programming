cf05562cbde0f61ac07250ea3c54d3c2e0c84974
LiJoBaZar <lijobazar@gmail.com>
2022-08-05 17:33:34 -0700

    Add logic at uint

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *binary_to_uint - Converts a binary number to an unsigned int.
+ *@b: String with number to convert
+ *Return: Decimal
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int dec = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec = 2 * dec + (b[i] - '0');
+	}
+	return (dec);
+}
